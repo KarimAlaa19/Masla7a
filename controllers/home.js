9cbd8eeaabd6ba8017076adbebdcf14486b11eb0
reem994 <59031503+reem994@users.noreply.github.com>
2021-06-26 22:02:20 +0200

    returning home data

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,8 @@ const User = require("../models/user-model");
 exports.homePage = async (req, res) => {
 
     const users = await User.find()
-      .populate("users");
+      .populate("users")
+      .select("name _id userName profilePic role  ");
     res.status(200).json({ users });
   };
   
