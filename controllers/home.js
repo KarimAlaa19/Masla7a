0435c96c40947f811494121c663d7da3ed36d6e7
reem994 <59031503+reem994@users.noreply.github.com>
2021-06-26 22:08:43 +0200

    excluding admin from users in home page

--- controllers/home.js
+++ controllers/home.js
@@ -2,9 +2,9 @@ const User = require("../models/user-model");
 
 exports.homePage = async (req, res) => {
 
-    const users = await User.find()
+    const users = await User.find({ role: { $ne: 'admin' } })
       .populate("users")
-      .select("name _id userName profilePic role  ");
-    res.status(200).json({ users });
+      .select("-password");
+    res.status(200).json({users: users });
   };
   
