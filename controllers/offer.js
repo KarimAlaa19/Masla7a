73e48553878a76cfe0ee0cb47be7293f487770a9
reem994 <59031503+reem994@users.noreply.github.com>
2021-07-25 16:41:45 +0200

    fixing deployment

--- controllers/offer.js
+++ controllers/offer.js
@@ -1,9 +1,9 @@
-const {Offer} = require("../models/offers");
+const { Offer } = require("../models/offers");
 const Order = require("../models/order-model");
 const User = require("../models/user-model");
 const service = require("../models/service-model");
-const {validateOffer} = require("../validators/offer-validator");
-
+const _ = require('lodash')
+const { validateOffer } = require("../validators/offer-validator");
 
 exports.addAnOffer = async (req, res) => {
   const serviceProvider = await User.findById(req.user._id)
@@ -13,37 +13,51 @@ exports.addAnOffer = async (req, res) => {
   const { error } = await validateOffer(req.body);
   if (error) return res.status(400).json(error.details[0].message);
 
-  console.log(Offer)
-  let offer = await Offer.findOne({serviceProvider:serviceProvider._id,status:'Valid'})
-  if(offer)
-  return res.status(400).json('You already have an offer avaliable')
+  console.log(Offer);
+  let offer = await Offer.findOne({
+    serviceProvider: serviceProvider._id,
+    status: "Valid",
+  });
+  if (offer) return res.status(400).json("You already have an offer avaliable");
 
   const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() + req.body.daysValidFor)
+  currentDate.setDate(currentDate.getDate() + req.body.daysValidFor);
 
-  const expiryDate =new Date(currentDate)
-  console.log(typeof expiryDate)
-   offer = await new Offer({
-      service: serviceProvider.serviceId._id,
-      serviceProvider: serviceProvider._id,
-      percentage: req.body.percentage,
-      daysValidFor : req.body.daysValidFor,
-      expireAt : expiryDate
-  })
+  const expiryDate = new Date(currentDate);
+  console.log(typeof expiryDate);
+  offer = await new Offer({
+    service: serviceProvider.serviceId._id,
+    serviceProvider: serviceProvider._id,
+    percentage: req.body.percentage,
+    daysValidFor: req.body.daysValidFor,
+    expireAt: expiryDate,
+  });
   await offer.save();
   res.status(200).json(offer);
 };
 
-exports.fetchAllOffers = async (req, res)=>{
-  const offers = await Offer.find()
+exports.fetchAllOffers = async (req, res) => {
+  let offers = await Offer.find();
   const todayDate = new Date();
-  offers.map(async (offer)=>{
-    if(offer.status ==='Valid'){
-      if(offer.expiryDate <todayDate )
-      offer.status = 'Expired'
-      await offer.save();
+  offers.map(async (offer) => {
+    if (offer.status === "Valid") {
+      if (offer.expireAt < todayDate) {
+        console.log("yaaay");
+        offer.status = "Expired";
+        await offer.save();
+        offer=null
+      }
     }
-  })
-  const validOffers = await Offer.find({status:'Valid'});
-  return res.status(200).json(validOffers)
-}
+  });
+  const index = _.findKey(
+    offers,
+    _.matchesProperty("status", 'Expired')
+  );
+  if (index !== undefined) {
+    offers.splice(index, 1);
+  }
+
+  console.log(offers)
+
+  return res.status(200).json({offers});
+};
